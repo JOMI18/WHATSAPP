d25a8a66d8ac00ce33ca7872e1ee74f97760fe69
Jomiii <getjommy@gmail.com>
2022-10-05 18:22:09 +0100

    fixed

---
+++ script.js
@@ -0,0 +1,34 @@
+// message1.addEventListener("click",Click1);
+// let display = document.getElementById('display1')
+
+// function Click1() {
+//     display.hidden=false;
+// }
+
+returnit.addEventListener("click",Returnit)
+
+function Returnit() {
+    document
+    document.getElementsByClassName("display").style.display="none"
+//    document.getElementById('display1').style.display="none"
+}
+
+
+
+// message2.addEventListener("click",Click1);
+
+//  let display2 = document.getElementById('display2')
+
+// function Click1() {
+//     display2.hidden=false;
+ 
+// }
+
+// returnit.addEventListener("click",Returnit)
+
+// function Returnit() {
+  
+//    document.getElementById('display2').style.display="none"
+// }
+
+
